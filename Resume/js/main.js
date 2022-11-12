5de333703ff472fb51e39d1b6bb1daed242fc5e5
Alexander234zxzx <118043961+Alexander234zxzx@users.noreply.github.com>
2022-11-12 11:44:24 +0400

    init

---
+++ Resume/js/main.js
@@ -0,0 +1,6 @@
+// const btnNode = document.querySelector('.js-btn');
+// const textWrapperNode = document.querySelector('.js-text-wrapper');
+
+// btnNode.addEventListener('click', () => {
+// 	textWrapperNode.innerHTML = 'Привет';
+// })
